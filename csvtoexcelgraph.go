b6f1a8d61aa37c4995695f3b8601efb1fde2fc41
tanaton <heiwaboke@gmail.com>
2020-03-26 11:58:43 +0900

    とりあえず動くようになった

---
+++ csvtoexcelgraph.go
@@ -0,0 +1,491 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+
+	"github.com/lxn/walk"
+	"github.com/lxn/walk/declarative"
+	"github.com/tanaton/CSVToExcelGraph/format"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var pngEncoder = png.Encoder{
+	CompressionLevel: png.BestCompression,
+	BufferPool:       NewPngPool(),
+}
+var rCRLF = strings.NewReplacer(
+	"\r\n", "\r\n",
+	"\n", "\r\n",
+)
+
+type Graph struct {
+	yaxislist []int
+	colmap    map[int]struct{}
+}
+
+type Column struct {
+	Title string `json:",omitempty"`
+	YAxis bool   `json:",omitempty"`
+}
+
+type Config struct {
+	XAxisNo int
+	Columns map[string]Column
+
+	cdir     string
+	current  string
+	namelist []string
+	namemap  map[string]string
+}
+
+type MyMainWindow struct {
+	*walk.MainWindow
+	xcComboConfigList *walk.ComboBox
+	xcEditConfig      *walk.TextEdit
+	xcImage           *walk.ImageView
+	xcEditLog         *walk.TextEdit
+	config            *Config
+	converting        bool
+}
+
+var log *zap.SugaredLogger
+
+func init() {
+	updateLogger(os.Stderr)
+}
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Warnw("panic!!!", "error", err)
+			os.Exit(1)
+		}
+	}()
+	os.Exit(_main())
+}
+
+func _main() int {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// カレントディレクトリ取得
+	cdir, err := os.Getwd()
+	if err != nil {
+		log.Warnw("カレントディレクトリの取得に失敗", "error", err)
+		return 1
+	}
+	if len(os.Args) > 1 {
+		// CUIで動作
+		err = cui(ctx, cdir, os.Args[1])
+	} else {
+		// GUI起動
+		err = gui(ctx, cdir)
+	}
+	if err != nil {
+		log.Warnw("ツール起動に失敗", "error", err)
+		return 1
+	}
+	return 0
+}
+
+func updateLogger(out io.Writer) {
+	if log != nil {
+		log.Sync()
+	}
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.AddSync(out),
+		zap.InfoLevel,
+	))
+	log = logger.Sugar()
+}
+
+func cui(ctx context.Context, cdir, rp string) error {
+	config := NewConfig(cdir)
+	err := config.Load()
+	if err != nil {
+		return err
+	}
+	if _, err := config.CreateGraph(rp); err != nil {
+		log.Warnw("グラフ生成失敗", "error", err)
+	} else {
+		log.Infow("グラフ生成成功")
+	}
+	return nil
+}
+
+func gui(ctx context.Context, cdir string) error {
+	config := NewConfig(cdir)
+	err := config.Load()
+	if err != nil {
+		log.Warnw("設定ファイルの読み込みに失敗しました。", "error", err)
+		return err
+	}
+	mmw := &MyMainWindow{
+		config:     config,
+		converting: false,
+	}
+	// ログ更新
+	updateLogger(mmw)
+	defer log.Sync()
+	// ダイアログオブジェクト生成
+	dialog := declarative.MainWindow{
+		AssignTo: &mmw.MainWindow,
+		Title:    "CSVToExcelGraph",
+		Size:     declarative.Size{Width: 800, Height: 600},
+		Layout:   declarative.VBox{},
+		OnDropFiles: func(files []string) {
+			if mmw.converting {
+				log.Infow("グラフ生成中の新しいドロップは無視されます。")
+			} else if checkExtList(files, ".csv") {
+				mmw.converting = true
+				num := runtime.NumCPU()
+				log.Infow("ファイルがドロップされました。", "ファイル数", len(files), "並列数", num)
+				// メッセージループを止めないようにgoroutineを起動させる
+				go func(files []string, num int) {
+					c := make(chan struct{}, num)
+					defer func() {
+						close(c)
+						mmw.converting = false
+					}()
+					for _, file := range files {
+						c <- struct{}{}
+						go func(file string) {
+							defer func() {
+								<-c
+							}()
+							mmw.CreateGraph(file)
+						}(file)
+					}
+					for num > 0 {
+						num--
+						c <- struct{}{}
+					}
+				}(files, num)
+			} else {
+				log.Infow("csv以外の拡張子のファイルはグラフ化できません。")
+			}
+		},
+		Children: []declarative.Widget{
+			declarative.ComboBox{
+				AssignTo:     &mmw.xcComboConfigList,
+				Model:        mmw.config.GetNameList(),
+				CurrentIndex: 0, // 初期値
+				OnCurrentIndexChanged: func() {
+					txt := mmw.xcComboConfigList.Text()
+					mmw.ReadConfigName(txt)
+				},
+			},
+			declarative.HSplitter{
+				Children: []declarative.Widget{
+					declarative.TextEdit{
+						AssignTo:  &mmw.xcEditConfig,
+						VScroll:   true,
+						MaxLength: 0x7FFFFFFF,
+						Text:      rCRLF.Replace(mmw.config.Text()),
+					},
+					declarative.ImageView{
+						AssignTo:   &mmw.xcImage,
+						Background: declarative.SolidColorBrush{Color: walk.RGB(255, 191, 0)},
+						Margin:     3,
+						Mode:       declarative.ImageViewModeZoom,
+						MinSize:    declarative.Size{Width: 580, Height: 300},
+					},
+				},
+			},
+			declarative.TextEdit{
+				AssignTo:  &mmw.xcEditLog,
+				VScroll:   true,
+				MaxLength: 0x7FFFFFFF,
+			},
+			declarative.PushButton{
+				Text: "ファイルを選択する",
+				OnClicked: func() {
+					dlg := &walk.FileDialog{}
+					dlg.FilePath = ""
+					dlg.Title = "グラフ化したいCSVを選択してください"
+					dlg.Filter = "Exe files (*.csv)|*.csv|All files (*.*)|*.*"
+					if ok, err := dlg.ShowOpen(mmw); err != nil {
+						log.Warnw("選択したファイルが開けませんでした。", "error", err)
+					} else if !ok {
+						log.Infow("キャンセルしました。")
+					} else {
+						rp := dlg.FilePath
+						log.Infow("選択", "path", rp)
+						mmw.CreateGraph(rp)
+					}
+				},
+			},
+		},
+	}
+	_, err = dialog.Run()
+	return err
+}
+
+func (mmw *MyMainWindow) Write(b []byte) (n int, err error) {
+	str := rCRLF.Replace(string(b))
+	// メッセージループが重い処理等で停止していると制御が戻らなくなるので注意
+	// Synchronizeを挟むと回避できるが、メッセージループが停止する事自体が問題なのでそっちで気を付ける
+	//mmw.MainWindow.Synchronize(func() { mmw.xcEditLog.AppendText(str) })
+	mmw.xcEditLog.AppendText(str)
+	return len(b), nil
+}
+
+func (mmw *MyMainWindow) ReadConfigName(name string) {
+	mmw.config.SetCurrent(name)
+	err := mmw.config.Load()
+	if err != nil {
+		mmw.xcEditConfig.SetText("error")
+		log.Warnw("設定ファイル読み込み異常", "error", err)
+	} else {
+		mmw.xcEditConfig.SetText(rCRLF.Replace(mmw.config.Text()))
+		log.Infow("設定ファイルの読み込みができました。")
+	}
+}
+
+func (mmw *MyMainWindow) CreateGraph(rp string) {
+	log.Infow("グラフ生成開始", "path", rp)
+	if ip, err := mmw.config.CreateGraph(rp); err != nil {
+		log.Warnw("グラフ生成異常", "error", err)
+	} else {
+		img, err := walk.NewImageFromFileForDPI(ip, 96)
+		if err != nil {
+			log.Warnw("グラフ画像生成失敗", "error", err)
+		} else {
+			mmw.xcImage.SetImage(img)
+			log.Infow("グラフ変換が完了しました。")
+		}
+	}
+}
+
+func checkExtList(list []string, ext string) bool {
+	if len(list) <= 0 {
+		return false
+	}
+	for _, name := range list {
+		if ext != filepath.Ext(name) {
+			return false
+		}
+	}
+	return true
+}
+
+func NewConfig(cdir string) *Config {
+	c := &Config{
+		cdir: cdir,
+	}
+	c.init()
+	return c
+}
+
+func (c *Config) init() {
+	rowlist, err := filepath.Glob(filepath.Join(c.cdir, "*.json"))
+	if err != nil || len(rowlist) == 0 {
+		log.Warnw("設定ファイルが見つかりませんでした。", "error", err)
+		rowlist = []string{}
+	}
+	c.namelist = []string{}
+	c.namemap = make(map[string]string)
+	if len(rowlist) > 0 {
+		c.current = rowlist[0]
+		for _, path := range rowlist {
+			_, name := filepath.Split(path)
+			c.namelist = append(c.namelist, name)
+			c.namemap[name] = path
+		}
+	}
+}
+
+func (c Config) GetNameList() []string {
+	return c.namelist
+}
+
+func (c *Config) SetCurrent(name string) {
+	p, ok := c.namemap[name]
+	if ok {
+		c.current = p
+	} else {
+		log.Warnw("選択された設定ファイルが無いよ", "filename", name)
+	}
+}
+
+func (c *Config) Load() error {
+	return c.ReadFile(c.current)
+}
+
+func (c *Config) ReadFile(p string) error {
+	rfp, err := os.Open(p)
+	if err != nil {
+		return err
+	}
+	defer rfp.Close()
+	dec := json.NewDecoder(rfp)
+	return dec.Decode(c)
+}
+func (c Config) WriteFile(p string) error {
+	wfp, err := os.Create(p)
+	if err != nil {
+		return err
+	}
+	defer wfp.Close()
+	enc := json.NewEncoder(wfp)
+	return enc.Encode(c)
+}
+func (c Config) Text() string {
+	txt, _ := json.MarshalIndent(c, "", "  ")
+	return string(txt)
+}
+func (c Config) CreateGraph(rp string) (string, error) {
+	dir, name := filepath.Split(rp)
+	csvname := strings.TrimRight(name, filepath.Ext(rp)) + "_graph.csv"
+	dp, _ := filepath.Abs(filepath.Join(dir, csvname))
+	// 間引き
+	g, err := c.ReduceCSV(rp, dp)
+	if err != nil {
+		return "", err
+	}
+	wp := dp + ".xlsx"
+	ip := wp + ".png"
+	// スレッドを固定する
+	runtime.LockOSThread()
+	// グラフ描画
+	err = format.Excelgraph(dp, wp, ip, g.yaxislist)
+	// スレッドの固定を解除する
+	runtime.UnlockOSThread()
+	os.Remove(dp)
+	if err != nil {
+		return "", err
+	}
+	// 生成されたpngの圧縮率が微妙なので再圧縮
+	err = regenePNG(ip)
+	return ip, err
+}
+func (c Config) ReduceCSV(rp, wp string) (*Graph, error) {
+	rfp, err := os.Open(rp)
+	if err != nil {
+		return nil, err
+	}
+	defer rfp.Close()
+	wfp, werr := os.Create(wp)
+	if werr != nil {
+		return nil, werr
+	}
+	defer wfp.Close()
+	// 各種初期化
+	hmax := 0
+	g := &Graph{}
+	g.colmap = make(map[int]struct{})
+	g.yaxislist = []int{}
+
+	r := bufio.NewScanner(rfp)
+	// ヘッダー
+	if r.Scan() {
+		line := r.Text()
+		headers := []string{}
+		cells := strings.Split(line, ",")
+		for i, it := range cells {
+			col, ok := c.Columns[formatColumn(uint64(i))]
+			if i == c.XAxisNo {
+				g.colmap[i] = struct{}{}
+				headers = append(headers, "xaxis")
+			} else if ok {
+				g.colmap[i] = struct{}{}
+				if col.YAxis {
+					g.yaxislist = append(g.yaxislist, len(headers))
+				}
+				headers = append(headers, it)
+			}
+		}
+		hmax = len(cells)
+		fmt.Fprint(wfp, strings.Join(headers, ",")+"\r\n")
+	} else {
+		return nil, errors.New("読み取れなかったよ")
+	}
+	// データ
+	for r.Scan() {
+		cells := strings.Split(r.Text(), ",")
+		if len(cells) < hmax-1 {
+			return nil, errors.New("csvのカンマの数が少ないよ")
+		}
+		lines := []string{}
+		for i, it := range cells {
+			if _, ok := g.colmap[i]; ok {
+				lines = append(lines, it)
+			}
+		}
+		fmt.Fprint(wfp, strings.Join(lines, ",")+"\r\n")
+	}
+	return g, nil
+}
+
+const digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// Go標準ライブラリ[src/strconv/itoa.go formatBits]から丸パクリ
+func formatColumn(u uint64) string {
+	var a [64 + 1]byte
+	i := len(a)
+	for u >= 26 {
+		i--
+		q := u / 26
+		a[i] = digits[uint(u-q*26)]
+		u = q
+	}
+	i--
+	a[i] = digits[uint(u)]
+	return string(a[i:])
+}
+
+func regenePNG(ip string) error {
+	img, err := openImage(ip)
+	if err != nil {
+		return err
+	}
+	wp, err := os.Create(ip)
+	if err != nil {
+		return err
+	}
+	defer wp.Close()
+	return pngEncoder.Encode(wp, img)
+}
+
+func openImage(ip string) (image.Image, error) {
+	rp, err := os.Open(ip)
+	if err != nil {
+		return nil, err
+	}
+	defer rp.Close()
+	return png.Decode(rp)
+}
+
+type PngPool struct {
+	pool sync.Pool
+}
+
+func NewPngPool() *PngPool {
+	return &PngPool{
+		pool: sync.Pool{
+			New: func() interface{} {
+				return &png.EncoderBuffer{}
+			},
+		},
+	}
+}
+func (p *PngPool) Get() *png.EncoderBuffer {
+	return p.pool.Get().(*png.EncoderBuffer)
+}
+func (p *PngPool) Put(buf *png.EncoderBuffer) {
+	p.pool.Put(buf)
+}
---
+++ format/excel.go
@@ -0,0 +1,251 @@
+package format
+
+import (
+	"errors"
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	ole "github.com/go-ole/go-ole"
+	"github.com/tanaton/go-ole-msoffice/excel"
+)
+
+type ExcelGraph struct {
+	obj *excel.Application
+}
+
+type GraphItem struct {
+	x     int
+	count int
+	rg    *excel.Range
+	leg   []string
+}
+
+// 終了処理
+func (ex *ExcelGraph) quit() {
+	ex.obj.Quit()
+	ex.obj.Release()
+}
+
+// 既存のブックを開く
+func (ex *ExcelGraph) openFile(p string) *excel.Workbook {
+	workbooks := ex.obj.GetWorkbooks()
+	workbook := workbooks.Open(p)
+	return workbook
+}
+
+// ブックを保存
+func (ex *ExcelGraph) saveBook(book *excel.Workbook, p string) {
+	ex.obj.SetDisplayAlerts(false)
+	book.SaveAs(p, excel.XlWorkbookDefault)
+}
+
+// グラフの作成
+func (ex *ExcelGraph) createChartObject(sheet *excel.Worksheet, x, y, xx, yy int, name string) *excel.ChartObject {
+	chs := sheet.ChartObjects()
+	g := chs.Add(x, y, xx, yy)
+	g.SetName(name)
+	return g
+}
+
+// グラフに描画するためのデータを取得
+func (ex *ExcelGraph) getGraphRange(sheet *excel.Worksheet) []GraphItem {
+	x := 1
+	arr := []GraphItem{}
+	maxCol := sheet.GetCells().GetItem(1, sheet.GetColumns().GetCount()).GetEnd(excel.XlToLeft).GetColumn() + 1
+
+	// 気合で探索
+	for {
+		v := sheet.GetCells().GetItem(1, x).GetValue()
+		if v.Value() == nil || sheet.Err != nil {
+			break
+		}
+
+		// 列の探索
+		leg := []string{}
+		i := x + 1
+		for ; i < maxCol; i++ {
+			v := sheet.GetCells().GetItem(1, i).GetValue()
+			if v.Value() == nil || sheet.Err != nil {
+				break
+			} else {
+				leg = append(leg, v.ToString())
+			}
+		}
+
+		item := GraphItem{
+			x:     x,                       // データ開始位置
+			count: ((i - 1) - (x + 1)) + 1, // データの列数
+			rg:    sheet.GetRange(sheet.GetColumns().GetItem(x+1), sheet.GetColumns().GetItem(i-1)),
+			leg:   leg,
+		}
+		arr = append(arr, item)
+		x = i + 1
+	}
+	return arr
+}
+
+// シートからグラフを作る
+func (ex *ExcelGraph) sheetToChart(g *excel.ChartObject, sheet *excel.Worksheet, secondary []int) {
+	j := 1
+	arr := ex.getGraphRange(sheet)
+	if len(arr) <= 0 {
+		fmt.Println("シートにグラフ化できるデータが無いみたい")
+		return
+	}
+
+	priname := []string{}
+	secname := []string{}
+	sec := map[int]struct{}{}
+	for _, it := range secondary {
+		sec[it] = struct{}{}
+	}
+	// 一つのレンジにまとめる
+	union := arr[0].rg
+	for i := 1; i < len(arr); i++ {
+		union = ex.obj.Union(union, arr[i].rg)
+	}
+
+	chart := g.GetChart()
+	// データの設定
+	chart.SetSourceData(union, excel.XlColumns)
+	// グラフの種類を設定
+	chart.SetChartType(excel.XlXYScatterLinesNoMarkers)
+	// 凡例の位置を修正
+	legend := chart.GetLegend()
+	legend.SetPosition(excel.XlLegendPositionBottom)
+	// 要素の設定
+	for _, it := range arr {
+		xcell := sheet.GetCells().GetItem(2, it.x)
+		for k := 1; k <= it.count; k++ {
+			// 線ごとにX軸の設定
+			sc := chart.SeriesCollection().Item(j)
+			if _, ok := sec[j]; ok {
+				// 2軸
+				sc.SetAxisGroup(excel.XlSecondary)
+				secname = append(secname, it.leg[k-1])
+			} else {
+				priname = append(priname, it.leg[k-1])
+			}
+			end := xcell.GetEnd(excel.XlDown)
+			rg := sheet.GetRange(xcell, end)
+			sc.SetXValues(rg)
+			j++
+		}
+	}
+	// グラフの軸についての設定
+	ex.setGraphAxis(g, strings.Join(priname, " / "))
+	// 指定した要素を第二軸へ移動
+	if len(secondary) > 0 {
+		ex.setGraphAxisSecondary(g, strings.Join(secname, " / "))
+	}
+}
+
+// グラフの軸を設定
+func (ex *ExcelGraph) setGraphAxis(g *excel.ChartObject, name string) {
+	chart := g.GetChart()
+	cp := chart.Axes(excel.XlCategory, excel.XlPrimary)
+	vp := chart.Axes(excel.XlValue, excel.XlPrimary)
+
+	// X軸の目盛線の表示
+	cp.SetHasMajorGridlines(true)
+	cp.SetHasMinorGridlines(true)
+	// Y軸の目盛線の表示
+	vp.SetHasMinorGridlines(true)
+	// 目盛線の位置を下に移動
+	cp.SetTickLabelPosition(excel.XlTickLabelPositionLow)
+	// X軸ラベルを表示
+	//cp.SetHasTitle(true)
+	//cat := cp.GetAxisTitle()
+	//cat.SetText("経過時間 (秒)")
+	// Y軸ラベルを表示
+	vp.SetHasTitle(true)
+	vat := vp.GetAxisTitle()
+	vat.SetText(name)
+}
+
+// 指定した要素を第二軸に移動
+func (ex *ExcelGraph) setGraphAxisSecondary(g *excel.ChartObject, name string) {
+	chart := g.GetChart()
+	// 2軸目のY軸ラベルを表示
+	vs := chart.Axes(excel.XlValue, excel.XlSecondary)
+	vs.SetHasTitle(true)
+	at := vs.GetAxisTitle()
+	at.SetText(name)
+}
+
+// タイトルを設定
+func (ex *ExcelGraph) setGraphTitle(g *excel.ChartObject, title string) {
+	chart := g.GetChart()
+	chart.SetHasTitle(true)
+	ct := chart.GetChartTitle()
+	ct.SetText(title)
+	ct.SetPosition(excel.XlChartElementPositionAutomatic)
+	ct.SetIncludeInLayout(false) // タイトルをグラフと重ねる
+}
+
+// グラフを画像にして保存
+func (ex *ExcelGraph) saveGraphImage(chart *excel.Chart, ip string) {
+	chart.Export(ip, "PNG")
+}
+
+// グラフを新しいシートに移動
+func (ex *ExcelGraph) moveNewGraphSheet(g *excel.ChartObject, name string) *excel.Chart {
+	chart := g.GetChart()
+	return chart.Location(excel.XlLocationAsNewSheet, name)
+}
+
+// スクリーン更新停止
+func (ex *ExcelGraph) lockScreen() {
+	ex.obj.SetScreenUpdating(false)
+}
+
+// スクリーン更新許可
+func (ex *ExcelGraph) unlockScreen() {
+	ex.obj.SetScreenUpdating(true)
+}
+
+func Excelgraph(rp, wp, ip string, secondary []int) (err error) {
+	// COMの初期化
+	ole.CoInitializeEx(0, ole.COINIT_APARTMENTTHREADED|ole.COINIT_DISABLE_OLE1DDE)
+	// 確実に行う必要があるため
+	defer ole.CoUninitialize()
+
+	// エクセルオブジェクトの生成
+	e := excel.ThisApplication()
+	if e == nil {
+		err = errors.New("Excelの起動に失敗しました")
+		return
+	}
+	ex := ExcelGraph{obj: e}
+	// Excelを閉じてリソース解放
+	defer ex.quit()
+	// 生成してる感を出すためアプリケーションを表示する
+	//ex.obj.SetVisible(true)
+	// 既存のブックの読み込み
+	book := ex.openFile(rp)
+	// シートの取得
+	sheet := book.GetWorksheets().GetItem(1)
+
+	ex.lockScreen()
+	// 空グラフの生成
+	graph := ex.createChartObject(sheet, 30, 30, 500, 300, "csvexcelgraph")
+	// シート内容をグラフに変換
+	ex.sheetToChart(graph, sheet, secondary)
+	// タイトルを設定
+	_, name := filepath.Split(rp)
+	ex.setGraphTitle(graph, strings.TrimSuffix(name, filepath.Ext(name)))
+	// グラフオブジェクトをグラフシートに移動
+	chart := ex.moveNewGraphSheet(graph, "Graph1")
+	ex.unlockScreen()
+
+	if ip != "" {
+		// グラフを画像で保存
+		ex.saveGraphImage(chart, ip)
+	}
+	// ブックを保存
+	ex.saveBook(book, wp)
+	// ブックを閉じる
+	book.Close()
+	return
+}
